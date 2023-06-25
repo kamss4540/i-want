0aeaa497a93d559f063b7df59083cd14b6f1e773
pengfei <pengfei@yunlizhihui.com>
2023-06-25 15:35:23 +0800

    引入elementUI

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <template>
-    <div>
-        npm
-    </div>
+	<div class="common-layout">
+		<el-container class="">
+			<el-header><Header /></el-header>
+			<el-container>
+				<el-aside width="200px"><Aside /></el-aside>
+				<el-main>Main</el-main>
+			</el-container>
+		</el-container>
+	</div>
 </template>
 
 <script setup lang="ts">
-
+import Header from "@/layout/Header.vue";
+import Aside from '@/layout/Aside.vue'
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.common-layout {
+	height: 100%;
+}
+.el-container {
+	height: 100%;
+}
 </style>
